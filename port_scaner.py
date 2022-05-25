e8dadc6ceee8454556dd6e871966d8e488e0d8ea
kali <zavlov_rf@mail.ru>
2022-05-25 17:45:30 +0500

    refactoring

--- port_scaner.py
+++ port_scaner.py
@@ -1,12 +1,10 @@
 import argparse
-import os
 import socket
 from queue import Queue
-from threading import Lock, Thread
+from threading import Thread
 
-N_THREADS = 1
+N_THREADS = 200
 q = Queue()
-add_lock = Lock()
 TIMEOUT = 1
 
 ports_info = {}
@@ -16,9 +14,9 @@ def tcp_port_scan(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.settimeout(TIMEOUT)
-        s.connect((host, port))
+        s.connect((host_ip, port))
         ports_info[port] = None
-        s.sendall(b"a\n\n")
+        s.sendall(b"GET / HTTP/1.1\nhost: " + bytes(host, "utf-8") + b"\n\n")
         data = s.recv(1024)
         data_str = data.decode("utf-8")
         if "+OK" in data_str:
@@ -27,7 +25,7 @@ def tcp_port_scan(port):
             ports_info[port] = "IMAP"
         elif "220" in data_str:
             ports_info[port] = "SMTP"
-        if "HTTP" in data_str:
+        if "HTTP" in data_str and "HTTPS" not in data_str:
             ports_info[port] = "HTTP"
     except:
         pass
@@ -81,8 +79,6 @@ def print_info():
             else:
                 line = "TCP " + str(port)
             print(line)
-        elif udp:
-            print("UDP " + str(port))
 
 
 if __name__ == '__main__':
@@ -96,7 +92,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     tcp = args.tcp
     udp = args.udp
+    if udp:
+        print("Not implemented yet")
+        quit()
     host, port_range = args.host, args.port_range
+    host_ip = socket.gethostbyname(host)
     start_port, end_port = port_range[0], port_range[1]
     start_port, end_port = int(start_port), int(end_port)
     ports = [p for p in range(start_port, end_port + 1)]
